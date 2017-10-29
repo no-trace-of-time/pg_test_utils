cae74a9073815e87dcbafc3c136daec22cda0517
simonxu72 <simon.xu72@gmail.com>
2017-10-29 13:16:22 +0800

    remove unused functions: new, name...

--- src/pg_test_utils.erl
+++ src/pg_test_utils.erl
@@ -2,9 +2,7 @@
 
 %% API exports
 -export([
-  new/1
-  , name/1
-  , setup/1
+  setup/1
 ]).
 
 -export([
@@ -12,30 +10,9 @@
   , db_init/1
 ]).
 
--define(TEST_REPO, pg_test_utils_t_repo).
--define(TEST_REPO_TBL, mchants).
--define(TEST_MODEL, pg_test_utils_t_model).
--define(TEST_PROTOCOL, pg_test_utils_t_protocol_up_resp_pay).
-
 %%====================================================================
 %% API functions
 %%====================================================================
-new(repo) ->
-  pg_model:new(?TEST_REPO, #{id=>1, mcht_full_name=><<"full">>, mcht_short_name=><<"short">>, update_ts=>100});
-new(model) ->
-  pg_model:new(?TEST_MODEL, #{id=>1, mcht_full_name=><<"full">>, mcht_short_name=><<"short">>, update_ts=>100}).
-
-%%--------------------------------------------------------------------
-name(repo) ->
-  ?TEST_REPO;
-name(model) ->
-  ?TEST_MODEL;
-name(table) ->
-  ?TEST_REPO_TBL;
-name(protocol) ->
-  ?TEST_PROTOCOL.
-
-%%--------------------------------------------------------------------
 setup(mnesia) ->
   Dir = "/tmp/mnesia_test",
   os:cmd("mkdir " ++ Dir),
